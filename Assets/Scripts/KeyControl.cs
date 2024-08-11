23b1ea5ab8d7441d26723a61ce5faafd917942d7
Ekrem Taha Ünlü <ekremtahaunlu@users.noreply.github.com>
2024-08-11 22:30:19 +0300

    DoTween and WebGL publish added.

--- Assets/Scripts/KeyControl.cs
+++ Assets/Scripts/KeyControl.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 using System.Collections.Generic;
 
 public class KeyControl : MonoBehaviour
@@ -8,30 +9,43 @@ public class KeyControl : MonoBehaviour
 	{
 		public KeyCode key;
 		public AudioSource audioSource;
+		public SpriteRenderer spriteRenderer;
 	}
 
 	[SerializeField] private List<KeySound> keySounds = new List<KeySound>();
+	[SerializeField] private float colorChangeDuration = 0.1f;
 
-	private Dictionary<KeyCode, AudioSource> _keyMap;
+	private Dictionary<KeyCode, KeySound> keyMap;
 
 	private void Start()
 	{
-		_keyMap = new Dictionary<KeyCode, AudioSource>();
+		keyMap = new Dictionary<KeyCode, KeySound>();
 		foreach (var keySound in keySounds)
 		{
-			_keyMap[keySound.key] = keySound.audioSource;
+			keyMap[keySound.key] = keySound;
 		}
 	}
 
 	private void Update()
 	{
-		foreach (var kvp in _keyMap)
+		foreach (var kvp in keyMap)
 		{
 			if (Input.GetKeyDown(kvp.Key))
 			{
-				kvp.Value.Play();
+				PlaySoundAndAnimate(kvp.Value);
 				break;
 			}
 		}
+	}
+
+	private void PlaySoundAndAnimate(KeySound keySound)
+	{
+		keySound.audioSource.Play();
+		
+		Color originalColor = keySound.spriteRenderer.color;
+		Sequence sequence = DOTween.Sequence();
+        
+		sequence.Append(keySound.spriteRenderer.DOColor(Color.red, colorChangeDuration))
+			.Append(keySound.spriteRenderer.DOColor(originalColor, colorChangeDuration));
 	}
 }
